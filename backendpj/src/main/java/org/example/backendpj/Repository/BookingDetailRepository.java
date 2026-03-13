dbd681b3b4c9f9781d71e437133c74235a4ff52e
Quyn Quyn <monchanhonkai@gmail.com>
2026-03-13 22:55:10 +0700

    Clear and commit again

--- backendpj/src/main/java/org/example/backendpj/Repository/BookingDetailRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.backendpj.Repository;
-
-import org.example.backendpj.Entity.BookingDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingDetailRepository extends JpaRepository<BookingDetail, Integer> {}
--- backendpj/src/main/resources/static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (typeof applyRolePermissions === "function") {
-    applyRolePermissions();
-  }
-
-  if (typeof setupAccountFeatures === "function") {
-    setupAccountFeatures();
-  }
-});
