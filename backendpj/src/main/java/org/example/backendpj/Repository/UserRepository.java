d35b3767f5b5319fe3c7eedafa4fa0132c5b1037
ayuhieu <nhieu2823@gmail.com>
2026-03-13 22:07:24 +0700

    fix back end
    fix backend

---
+++ backendpj/src/main/java/org/example/backendpj/Repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.example.backendpj.Repository;
+
+import org.example.backendpj.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
