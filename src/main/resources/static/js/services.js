ce2ccdf51e0dfa06ab951be9c72ffb834c548113
caoson181 <sonphamcao2005@gmail.com>
2026-03-27 10:10:05 +0700

    Add effect to Services page, update infomation in Contact page

--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -2,32 +2,51 @@ const sections = document.querySelectorAll(".service-card");
 const navLinks = document.querySelectorAll(".nav-link");
 
 window.addEventListener("scroll", () => {
-    let current = "";
+  let current = "";
 
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 200;
-        const sectionHeight = section.clientHeight;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 200;
+    const sectionHeight = section.clientHeight;
 
-        if (window.scrollY >= sectionTop &&
-            window.scrollY < sectionTop + sectionHeight) {
-            current = section.getAttribute("id");
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-
-        if (link.getAttribute("href") === `#${current}`) {
-            link.classList.add("active");
-        }
-    });
-
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 50) {
-        navLinks.forEach(link => link.classList.remove("active"));
-
-        const lastLink = document.querySelector('a[href="#restaurant"]');
-        if (lastLink) {
-            lastLink.classList.add("active");
-        }
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      current = section.getAttribute("id");
     }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
+    navLinks.forEach((link) => link.classList.remove("active"));
+
+    const lastLink = document.querySelector('a[href="#restaurant"]');
+    if (lastLink) {
+      lastLink.classList.add("active");
+    }
+  }
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.25,
+  },
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
