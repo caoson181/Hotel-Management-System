d8702e4a9e93c0bf283e54408150f9187f11f200
Higanbana311 <vinhhoangxuan0311@gmail.com>
2026-04-06 09:23:58 +0700

    deploy render

--- src/main/java/org/example/backendpj/BackendpjApplication.java
+++ src/main/java/org/example/backendpj/BackendpjApplication.java
@@ -3,11 +3,14 @@ package org.example.backendpj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 @SpringBootApplication
 @EntityScan("org.example.backendpj.Entity")
 public class BackendpjApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendpjApplication.class, args);
+        SpringApplication app = new SpringApplication(BackendpjApplication.class);
+        app.setAdditionalProfiles("local");
+        app.run(args);
     }
 }
